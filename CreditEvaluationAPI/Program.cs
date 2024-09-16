044fea5b943b5522fe007bdbf743125bc5686c27
dericki <derick.araujoo@gmail.com>
2024-09-16 20:54:32 -0300

    chore: update imports and files

--- CreditEvaluationAPI/Program.cs
+++ CreditEvaluationAPI/Program.cs
@@ -2,8 +2,15 @@ using CreditEvaluationAPI.Repositorie;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MyDbContext>(options =>
+    options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection")));
+
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -14,6 +21,9 @@ if (app.Environment.IsDevelopment())
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
     });
 }
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
